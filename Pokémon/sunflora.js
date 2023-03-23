b263875ffdccdf6688143a539bfba96ae95888a2
Sofie Larsen <Sofieamalierl@hotmail.com>
2023-03-23 10:27:46 +0100

    potter app

--- Pokémon/sunflora.js
+++ Pokémon/sunflora.js
@@ -1,54 +1,73 @@
 "use script";
 
-const sunFlora = {
-    name: "Sunflora",
-    description: "Sunflora is a plant Pokémon with a sunflower-like design, having a green trunk and limbs",
-    image: "https://archives.bulbagarden.net/media/upload/0/0d/0192Sunflora.png",
-    footprint: "https://archives.bulbagarden.net/media/upload/f/f8/F0192.png",
-    dexindex:192,
-    type: "Sun",
-    subtype: "Grass",
-    weaknesses: "Fire, Ice, Poison, Bug, Flying",
-    gender: "Uni sex",
-    weight: 8500,
-    height: 80,
-    generation: "Gen. II-IV",
-    spilversion: "1 version",
-    canEvolve: "boolean",
-    statsHP:0,
-    statsAttack: 0,
-    statsDefence: 0,
-    statsSpecialAttack: 2,
-    statsSpecialDefence: 0,
-    statsSpeed: 0,
+window.addEventListener("load", initApp)
+
+async function initApp() {
+    document.querySelector("#dialog").close();
+    const pokemon = await getPokemon();
+    
+    showPokemon(pokemon);
+    showPokemon(pokemon);
+    showPokemon(pokemon);
+    showPokemon(pokemon);
+
 }
+async function getPokemon() {
+    const response = await fetch("sunflora.json")
+    const data = await response.json();
+    return data;
+}
+
 function showPokemon(pokemon) {
-    document.querySelector("#firstName").textContent = student.firstName
-    document.querySelector("#middelName").textContent = student.middelName
-    document.querySelector("#lastName").textContent = student.lastName
+    document.querySelector("#pokemon").insertAdjacentHTML("beforeend",
+    /*html*/`
+    <article>
+    <img src="${pokemon.image}">
+    <h2>${pokemon.name}</h2>
+    <p>#${pokemon.dexindex}</p>
+    <p>${pokemon.type}</p>
+    </article>
+    `)
+    document.querySelector("#pokemon article:last-child").addEventListener("click", pokemonClicked);
+    
+    function pokemonClicked() {
+        showPokemonModal(pokemon);
+        
+    }
 }
-function addPokemons(pokemon) {
-    const myHtml = /*html*/ `
-    <li>name ${pokemon.name}</li> 
-    <li>description ${pokemon.description}</li>
-    <li>image ${pokemon.image}</li>
-    <li>footprint ${pokemon.footprint}</li>
-    <li>dexindex ${pokemon.dexindex}</li>
-    <li>type ${pokemon.type}</li>
-    <li>subtype ${pokemon.subtype}</li>
-    <li>gender ${pokemon.gender}</li>
-    <li>weaknesse ${pokemon.weaknesses}</li>
-    <li>height ${pokemon.height}</li>
-    <li> weight ${pokemon.weight}</li>
-    <li>spilversion ${pokemon.spilversion}</li>
-    <li>canEvolve ${pokemon.canEvolve}</li>
-    <li>generation ${pokemon.generation}</li>
-    <li>statsAttack ${pokemon.statsAttack}</li>
-    <li>statsHP ${pokemon.statsHP}</li>
-    <li>statsSpecialAttack ${pokemon.statsSpecialAttack}</li>
-    <li>statsDefence ${pokemon.statsDefence}</li>
-    <li>statsSpeed ${pokemon.statsSpeed}</li>
-    <li>statsSpecialDefence ${pokemon.statsSpecialDefence}</li>
-    `;
-    document.querySelector("#pokemons").insertAdjacentHTML("beforeend", myHtml);
+
+function showPokemonModal(pokemon) {
+
+    document.querySelector("#dialog-name").textContent = pokemon.name;
+    document.querySelector("#dialog-description").textContent = pokemon.description;
+    document.querySelector("#dialog-ability").textContent = pokemon.ability;
+ 
+    document.querySelector("#dialog-pokemon").showModal();
 }
+
+
+// function addPokemons(pokemon) {
+//     const myHtml = /*html*/ `
+//     <li>name ${pokemon.name}</li> 
+//     <li>description ${pokemon.description}</li>
+//     <li>image ${pokemon.image}</li>
+//     <li>footprint ${pokemon.footprint}</li>
+//     <li>dexindex ${pokemon.dexindex}</li>
+//     <li>type ${pokemon.type}</li>
+//     <li>subtype ${pokemon.subtype}</li>
+//     <li>gender ${pokemon.gender}</li>
+//     <li>weaknesse ${pokemon.weaknesses}</li>
+//     <li>height ${pokemon.height}</li>
+//     <li> weight ${pokemon.weight}</li>
+//     <li>spilversion ${pokemon.spilversion}</li>
+//     <li>canEvolve ${pokemon.canEvolve}</li>
+//     <li>generation ${pokemon.generation}</li>
+//     <li>statsAttack ${pokemon.statsAttack}</li>
+//     <li>statsHP ${pokemon.statsHP}</li>
+//     <li>statsSpecialAttack ${pokemon.statsSpecialAttack}</li>
+//     <li>statsDefence ${pokemon.statsDefence}</li>
+//     <li>statsSpeed ${pokemon.statsSpeed}</li>
+//     <li>statsSpecialDefence ${pokemon.statsSpecialDefence}</li>
+//     `;
+//     document.querySelector("#pokemons").insertAdjacentHTML("beforeend", myHtml);
+// }
--- app.js
+++ app.js
@@ -1,39 +1,95 @@
-"use strict";
+"use strict"
 
-// ============ LOAD & INIT APP ============ //
 window.addEventListener("load", initApp);
 
-function initApp() {
-    // Harry Potter
-    const potterName = "Harry Potter";
-    const potterGender = "male";
-    const potterHouse = "Gryffindor";
-    const potterDateOfBirth = "31-07-1980";
-    const potterAncestry = "half-blood";
-    const potterEyeColour = "green";
-    const potterHairColour = "black";
-    const potterActor = "Daniel Radcliffe";
-    const potterImage = "http://hp-api.herokuapp.com/images/harry.jpg";
+function initApp(){
+// Harry Potter
+const harry = {
+    name: "Harry Potter",
+    species: "human",
+    gender: "male",
+    house: "Gryffindor",
+    dateOfBirth: "31-07-1980",
+    yearOfBirth: 1980,
+    ancestry: "half-blood",
+    eyeColour: "green",
+    hairColour: "black",
+    wand: "holly,phoenix feather,11",
+    patronus: "stag",
+    hogwartsStudent: true,
+    hogwartsStaff: false,
+    actor: "Daniel Radcliffe",
+    alive: true,
+    image: "http://hp-api.herokuapp.com/images/harry.jpg"
+};
+
+// Hermione Granger
+const hermione = {
+    name: "Hermione Granger",
+    species: "human",
+    gender: "female",
+    house: "Gryffindor",
+    dateOfBirth: "19-09-1979",
+    yearOfBirth: 1979,
+    ancestry: "muggleborn",
+    eyeColour: "brown",
+    hairColour: "brown",
+    wand: "vine,dragon heartstring",
+    patronus: "otter",
+    hogwartsStudent: true,
+    hogwartsStaff: false,
+    actor: "Emma Watson",
+    alive: true,
+    image: "http://hp-api.herokuapp.com/images/hermione.jpeg"
+};
+
+// Ron Weasley
+const ron = {
+    name: "Ron Weasley",
+    species: "human",
+    gender: "male",
+    house: "Gryffindor",
+    dateOfBirth: "01-03-1980",
+    yearOfBirth: 1980,
+    ancestry: "pure-blood",
+    eyeColour: "blue",
+    hairColour: "red",
+    wand: "willow,unicorn tail-hair,14",
+    patronus: "Jack Russell terrier",
+    hogwartsStudent: true,
+    hogwartsStaff: false,
+    actor: "Rupert Grint",
+    alive: true,
+    image: "http://hp-api.herokuapp.com/images/ron.jpg"
+};
+    console.log()   
+    showStudent(harry);
+    showStudent(ron);
+    showStudent(hermione);
+
     
-    console.log(
-        potterName,
-        potterGender,
-        potterHouse,
-        potterDateOfBirth,
-        potterAncestry,
-        potterEyeColour,
-        potterHairColour,
-        potterActor,
-        potterImage
-    );
-    
-    showCharacter(potterImage, potterName, potterHouse, potterDateOfBirth, potterActor);
 }
 
-function showCharacter(Image, Name, House, DateOfBirth, Actor) {
-    console.log(Image);
-    console.log(Name);
-    console.log(House);
-    console.log(DateOfBirth);
-    console.log(Actor); 
+function showStudent(student) {
+    const myHtml = /*html*/ `
+    <article class= "grid-item">
+        <img src="${student.image}">
+        <h2>${student.name}</h2>
+        <p>${student.house}</p>
+        <p>${student.dateOfBirth}</p>
+        <p>${student.actor}</p>
+    </article>
+    `;
+    document.querySelector("#characters").insertAdjacentHTML("beforeend", myHtml);
 }
+
+
+// function addStudents(student) {
+//     const myHtml = /*html*/ `
+//     <li>firstName ${student.firstName}</li>
+//     <li>middleName ${student.middleName}</li>
+//     <li>lastName ${student.lastName}</li>
+//     `;
+//     document.querySelector("#students").insertAdjacentHTML("beforeend", myHtml);
+
+// }
